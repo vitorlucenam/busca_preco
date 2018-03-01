b49c1e5d9b8f62efd1cce003691c279062b3f9d3
Eduardo B.Nunes <eduardobarroso@edu.unifor.br>
2018-03-01 01:08:07 -0300

    Mudanças na classe, remoção do email e idade

--- src/model/User.java
+++ src/model/User.java
@@ -2,20 +2,16 @@ package model;
 
 public class User {
     private String name;
-    private String email;
     private String user;
     private String password;
-    private int age;
     private int cep;
     private int id;
 
-    public User(String name, String email, String user, String password, int age, int cep, int id){
+    public User(String name, String user, String password, int cep, int id){
         super();
         this.name=name;
-        this.email=email;
         this.user=user;
         this.password=password;
-        this.age=age;
         this.cep=cep;
         this.id=id;
     }
@@ -25,14 +21,6 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getUser() {
@@ -49,14 +37,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public int getCep() {
